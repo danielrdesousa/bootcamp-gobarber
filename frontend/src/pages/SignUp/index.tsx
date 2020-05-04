bf6caf78a407752a356064f4919191c0c5fe074c
Daniel Rodrigues <danielrdesousa@gmail.com>
2020-05-04 01:20:15 -0300

    Create pages SignIn and SignUp

---
+++ frontend/src/pages/SignUp/index.tsx
@@ -0,0 +1,81 @@
+import React, { useCallback, useRef } from 'react';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um email válido'),
+        password: Yup.string().min(6, 'No mínimo 6 digitos'),
+      });
+
+      await schema.validate(data, { abortEarly: false });
+    } catch (err) {
+      const errors = getValidationErrors(err);
+
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
+  return (
+    <>
+      <Container>
+        <Background />
+
+        <Content>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1> Faça seu Cadastro </h1>
+
+            <Input
+              type="text"
+              name="name"
+              placeholder="Nome Completo"
+              icon={FiUser}
+            />
+            <Input
+              type="text"
+              name="email"
+              placeholder="E-mail"
+              icon={FiMail}
+            />
+            <Input
+              type="password"
+              name="password"
+              placeholder="Senha"
+              icon={FiLock}
+            />
+
+            <Button type="submit">Cadastrar</Button>
+          </Form>
+
+          <a href="#">
+            <FiArrowLeft />
+            Voltar para logon
+          </a>
+        </Content>
+      </Container>
+    </>
+  );
+};
+
+export default SignUp;
